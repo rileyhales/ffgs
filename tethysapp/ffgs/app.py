1b894b2a0aaa462585a434fa333e57d6afb6d363
rileyhales <rileyhales1@gmail.com>
2019-05-21 18:07:40 -0600

    FFGS commit

---
+++ tethysapp/ffgs/app.py
@@ -0,0 +1,34 @@
+from tethys_sdk.base import TethysAppBase, url_map_maker
+
+
+class Ffgs(TethysAppBase):
+    """
+    Tethys app class for FFGS Tool.
+    """
+
+    name = 'FFGS Tool'
+    index = 'ffgs:home'
+    icon = 'ffgs/images/ffgs.png'
+    package = 'ffgs'
+    root_url = 'ffgs'
+    color = '#008080'
+    description = 'An interface for viewing areas at risk for flood based on the FFGS'
+    tags = ''
+    enable_feedback = False
+    feedback_emails = []
+
+    def url_maps(self):
+        """
+        Add controllers
+        """
+        UrlMap = url_map_maker(self.root_url)
+
+        url_maps = (
+            UrlMap(
+                name='home',
+                url='ffgs',
+                controller='ffgs.controllers.home'
+            ),
+        )
+
+        return url_maps
---
+++ tethysapp/ffgs/public/js/main.js
@@ -0,0 +1,39 @@
+// Getting the csrf token
+let csrftoken = Cookies.get('csrftoken');
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+$.ajaxSetup({
+    beforeSend: function (xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
+
+
+// ////////////////////////////////////////////////////////////////////////  AJAX FUNCTIONS
+// function getThreddswms() {
+//     $.ajax({
+//         url: '/apps/gldas/ajax/getCustomSettings/',
+//         async: false,
+//         data: '',
+//         dataType: 'json',
+//         contentType: "application/json",
+//         method: 'POST',
+//         success: function (result) {
+//             threddsbase = result['threddsurl'];
+//             geoserverbase = result['geoserverurl']
+//         },
+//     });
+// }
+//
+// ////////////////////////////////////////////////////////////////////////  LOAD THE MAP
+// let threddsbase;
+// let geoserverbase;
+// getThreddswms();                        // sets the value of threddsbase and geoserverbase
+const mapObj = map();                   // used by legend and draw controls
+const basemapObj = basemaps();          // used in the make controls function

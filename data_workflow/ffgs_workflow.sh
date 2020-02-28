7bee9113601a71322ff87844788cf81ec2b5f02d
rileyhales <rileyhales1@gmail.com>
2020-02-28 13:45:26 -0700

    workflow environment.yml

--- data_workflow/ffgs_workflow.sh
+++ data_workflow/ffgs_workflow.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # activate the python environment containing the dependencies to run the workflow
-source /media/root/tethys/main/tethys/miniconda/etc/profile.d/conda.sh; conda activate tethys
+source /home/civil/miniconda3/etc/profile.d/conda.sh; conda activate workflow-ffgs
 # exectue the workflow using the path to the gfsworkflow.py file and the path to save the data
 python /home/civil/apps/ffgs/data_workflow/gfsworkflow.py /home/civil/thredds_data/ffgs/
 python /home/civil/apps/ffgs/data_workflow/wrfprworkflow.py /home/civil/thredds_data/ffgs/
